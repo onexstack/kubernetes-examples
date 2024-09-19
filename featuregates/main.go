eca32a913ec35a1a7b3f1f738fa78c96efb23895
孔令飞 <konglingfei.nosbelm@bytedance.com>
2024-09-20 01:45:52 +0800

    feat: commit at 20240920014552
    Change-Id: I2337ea2bce7fc7d6a35a8179044a1ab30f881d53

--- featuregates/main.go
+++ featuregates/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 
+	"github.com/spf13/pflag"
+
 	"github.com/superproj/k8sdemo/featuregates/feature"
 )
 
 func main() {
+	fs := pflag.NewFlagSet("feature", pflag.ExitOnError)
+	feature.DefaultMutableFeatureGate.AddFlag(fs)
+	pflag.Parse()
+
+	// 设置功能门控的值
+	//_ = feature.DefaultMutableFeatureGate.SetFromMap(*featureGates)
+
 	if feature.DefaultFeatureGate.Enabled(feature.MyNewFeature) {
 		// 启用新特性时的逻辑
 		// 例如，执行某些新功能的代码
